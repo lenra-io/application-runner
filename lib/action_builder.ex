1f603296410631f1aeba9a321dd0b918aab811a6
Shiipou <shiishii@nocturlab.fr>
2021-11-16 15:03:49 +0100

    feat: Load widget from application using manifest.

--- lib/action_builder.ex
+++ lib/action_builder.ex
@@ -13,15 +13,23 @@ defmodule ApplicationRunner.ActionBuilder do
     This function build the first UI with default Entry Point `"InitData"` to generate the data model and `"MainUi"` to generate the UI
   """
   def first_run(%AppContext{} = app_context) do
+    {:ok, %{"entrypoint" => entrypoint}} = get_manifest(app_context)
+
     UIValidator.get_and_build_widget(app_context, %WidgetContext{
-      widget_name: "root",
+      widget_name: entrypoint,
       prefix_path: "/"
     })
   end
 
   @behaviour ApplicationRunner.AdapterBehavior
-  defdelegate run_action(action),
+  defdelegate get_manifest(app),
     to: Application.fetch_env!(:application_runner, :adapter)
+
+  defdelegate get_widget(app, widget, data),
+    to: Application.fetch_env!(:application_runner, :adapter)
+
+  defdelegate run_listener(app, listener, data),
+      to: Application.fetch_env!(:application_runner, :adapter)
 
   defdelegate get_data(action), to: Application.fetch_env!(:application_runner, :adapter)
   defdelegate save_data(action, data), to: Application.fetch_env!(:application_runner, :adapter)
