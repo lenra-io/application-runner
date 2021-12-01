5cf51d3b769ebc2563b6873420e4d69a1855840b
Flavien CADET <shiishii@nocturlab.fr>
2021-12-01 15:57:54 +0100

    feat: Update dependencies (#38)

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule ApplicationRunner.MixProject do
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ex_component_schema,
-       git: "https://github.com/lenra-io/ex_component_schema", ref: "v1.0.0-beta.1"},
+       git: "https://github.com/lenra-io/ex_component_schema", ref: "v1.0.0-beta.2"},
       {:jason, "~> 1.2"},
       {:json_diff, "~> 0.1"}
     ]
