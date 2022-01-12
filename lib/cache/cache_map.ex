fcb1499caac409e9d092ce6c54ce9a60b14fd1c6
Louis G <louis.guilbert@lenra.io>
2022-01-12 10:29:09 +0100

    Update lib/cache/cache_map.ex
    Co-authored-by: Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>

--- lib/cache/cache_map.ex
+++ lib/cache/cache_map.ex
@@ -1,6 +1,6 @@
 defmodule ApplicationRunner.CacheMap do
   @moduledoc """
-    This is a simple using of the CacheMapMacro.
+    This is a simple use of the CacheMapMacro.
     This is used by the CacheAsyncMacro.
   """
   use ApplicationRunner.CacheMapMacro
