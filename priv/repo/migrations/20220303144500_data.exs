0c5ddf639cd5deef396df5170e3ba4f2083c0b3d
Emric Pichonnier <emric.pichonnier@gmail.com>
2022-03-03 16:23:45 +0100

    feat: add repositry (#85)

---
+++ priv/repo/migrations/20220303144500_data.exs
@@ -0,0 +1,43 @@
+defmodule ApplicationRunner.Repo.Migrations.Data do
+  use Ecto.Migration
+
+  def change do
+    create table(:environments) do
+      timestamps()
+    end
+
+    create table(:users) do
+      timestamps()
+    end
+
+    create table(:datastores) do
+      add(:environment_id, references(:environments), null: false)
+      add(:name, :string)
+
+      timestamps()
+    end
+
+    create(unique_index(:datastores, [:name, :environment_id], name: :datastores_name_application_id_index))
+
+    create table(:datas) do
+      add(:datastore_id, references(:datastores), null: false)
+      add(:data, :map, null: false)
+
+      timestamps()
+    end
+
+    create table(:user_datas) do
+      add(:user_id, references(:users), null: false)
+      add(:data_id, references(:data), null: false)
+
+      timestamps()
+    end
+
+    create table(:data_references) do
+      add(:refs_id, references(:data), null: false)
+      add(:refBy_id, references(:data), null: false)
+
+      timestamps()
+    end
+  end
+end
