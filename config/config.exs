c1475eb8862d995df91e9ad47d65659634e4a595
Emric Pichonnier <emric.pichonnier@lenra.me>
2022-03-07 16:54:34 +0100

    fix config

--- config/config.exs
+++ config/config.exs
@@ -12,7 +12,9 @@ config :application_runner,
   # 60 min
   env_inactivity_timeout: 1000 * 60 * 60,
   additional_app_modules: [],
-  additional_session_modules: []
+  additional_session_modules: [],
+  lenra_environment_schema: ApplicationRunner.FakeLenraEnvironment,
+  lenra_user_schema: ApplicationRunner.FakeLenraUser
 
 config :application_runner, ApplicationRunner.Repo,
   database: "file::memory:?cache=shared",
