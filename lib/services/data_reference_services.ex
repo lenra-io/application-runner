f024686857359add63d7462cc680f7e094f8769a
Emric Pichonnier <emric.pichonnier@lenra.me>
2022-03-08 16:10:31 +0100

    fix credo

--- lib/services/data_reference_services.ex
+++ lib/services/data_reference_services.ex
@@ -5,7 +5,7 @@ defmodule ApplicationRunner.DataReferenceServices do
 
   def create(op), do: Ecto.Multi.new() |> create(op)
 
-  def create(multi, %{refs: refs, refBy: refBy}) do
+  def create(multi, %{refs: refs, ref_by: ref_by}) do
     multi
     |> Ecto.Multi.run(:refs, fn repo, _params ->
       case repo.get(Data, refs) do
@@ -17,7 +17,7 @@ defmodule ApplicationRunner.DataReferenceServices do
       end
     end)
     |> Ecto.Multi.run(:refBy, fn repo, _params ->
-      case repo.get(Data, refBy) do
+      case repo.get(Data, ref_by) do
         nil ->
           {:error, :data_not_found}
 
@@ -25,8 +25,8 @@ defmodule ApplicationRunner.DataReferenceServices do
           {:ok, ref}
       end
     end)
-    |> Ecto.Multi.insert(:inserted_reference, fn %{refs: %Data{} = refs, refBy: %Data{} = refBy} ->
-      DataReferences.new(refs.id, refBy.id)
+    |> Ecto.Multi.insert(:inserted_reference, fn %{refs: %Data{} = refs, ref_by: %Data{} = ref_by} ->
+      DataReferences.new(refs.id, ref_by.id)
     end)
   end
 end
