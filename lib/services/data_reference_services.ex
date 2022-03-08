fb0cd2d99aee1642542653676699bbfcf758c7ed
Emric Pichonnier <emric.pichonnier@lenra.me>
2022-03-08 16:09:50 +0100

    feat: Add reference services

---
+++ lib/services/data_reference_services.ex
@@ -0,0 +1,32 @@
+defmodule ApplicationRunner.DataReferenceServices do
+  @moduledoc false
+
+  alias ApplicationRunner.{Data, DataReferences}
+
+  def create(op), do: Ecto.Multi.new() |> create(op)
+
+  def create(multi, %{refs: refs, refBy: refBy}) do
+    multi
+    |> Ecto.Multi.run(:refs, fn repo, _params ->
+      case repo.get(Data, refs) do
+        nil ->
+          {:error, :data_not_found}
+
+        ref ->
+          {:ok, ref}
+      end
+    end)
+    |> Ecto.Multi.run(:refBy, fn repo, _params ->
+      case repo.get(Data, refBy) do
+        nil ->
+          {:error, :data_not_found}
+
+        ref ->
+          {:ok, ref}
+      end
+    end)
+    |> Ecto.Multi.insert(:inserted_reference, fn %{refs: %Data{} = refs, refBy: %Data{} = refBy} ->
+      DataReferences.new(refs.id, refBy.id)
+    end)
+  end
+end
