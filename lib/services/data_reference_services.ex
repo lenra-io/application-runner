61e6d73966f9136829c11e0537cd9c1d622e835a
Emric Pichonnier <emric.pichonnier@lenra.me>
2022-03-15 09:54:51 +0100

    feat: Add delete to reference service (#102)

--- lib/services/data_reference_services.ex
+++ lib/services/data_reference_services.ex
@@ -11,4 +11,29 @@ defmodule ApplicationRunner.DataReferencesServices do
       DataReferences.new(params)
     end)
   end
+
+  def delete(params), do: Ecto.Multi.new() |> delete(params)
+
+  def delete(multi, %{refs_id: refs, refBy_id: refBy}) do
+    multi
+    |> Ecto.Multi.run(:reference, fn repo, _params ->
+      case repo.get_by(DataReferences, refs_id: refs, refBy_id: refBy) do
+        nil ->
+          {:error, :reference_not_found}
+
+        ref ->
+          {:ok, ref}
+      end
+    end)
+    |> Ecto.Multi.delete(:deleted_reference, fn %{reference: %DataReferences{} = reference} ->
+      reference
+    end)
+  end
+
+  def delete(multi, _params) do
+    multi
+    |> Ecto.Multi.run(:reference, fn _repo, _params ->
+      {:error, :json_format_invalid}
+    end)
+  end
 end
