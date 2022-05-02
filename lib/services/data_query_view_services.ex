eaedb09cd260035074285fd09a15f042d61bb865
Louis G <louis@lenra.me>
2022-05-02 13:45:07 +0200

    feat: Add service to get one or all DataQuery in DataQueryView

---
+++ lib/services/data_query_view_services.ex
@@ -0,0 +1,27 @@
+defmodule ApplicationRunner.DataQueryViewServices do
+  @moduledoc false
+  import Ecto.Query, only: [from: 2]
+
+  alias ApplicationRunner.{DataQueryView}
+
+  def get_one(env_id, datastore_name, data_id) do
+    from(
+      dv in DataQueryView,
+      where:
+        dv.environment_id == ^env_id and
+          dv.id == ^data_id and
+          fragment("data ->> '_datastore'") == ^datastore_name,
+      select: dv
+    )
+  end
+
+  def get_all(env_id, datastore_name) do
+    from(
+      dv in DataQueryView,
+      where:
+        dv.environment_id == ^env_id and
+          fragment("data ->> '_datastore'") == ^datastore_name,
+      select: dv
+    )
+  end
+end
