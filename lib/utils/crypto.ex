7f8ed4756828a90fe657386ef4e75d65a669547b
Emric Pichonnier <emric.pichonnier@lenra.io>
2022-05-10 12:17:12 +0200

    fix credo

--- lib/utils/crypto.ex
+++ lib/utils/crypto.ex
@@ -1,4 +1,7 @@
 defmodule Crypto do
+  @moduledoc """
+    This module hash tuple in sha256
+  """
   def hash(tuple) do
     :crypto.hash(:sha256, :erlang.term_to_binary(tuple))
     |> Base.encode64()
