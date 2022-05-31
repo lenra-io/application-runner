c23212b4e2deca44e9a778bda69efdf684da9f51
Emric Pichonnier <emric.pichonnier@lenra.io>
2022-05-31 17:08:52 +0200

    feat: add schema

---
+++ lib/db_schema/fake_lenra_user.ex
@@ -0,0 +1,24 @@
+defmodule ApplicationRunner.User do
+  @moduledoc """
+    The user schema.
+  """
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias ApplicationRunner.{
+    UserData
+  }
+
+  embedded_schema do
+    has_many(:user_datas, UserData, foreign_key: :user_id)
+    field(:email, :string)
+    timestamps()
+  end
+
+  def embede(user) do
+    %ApplicationRunner.User{}
+    |> cast(user, [:email])
+    |> validate_required([:email])
+  end
+end
