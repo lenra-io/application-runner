07589804fb16886cb005df1466900bd24596f7dc
Emric Pichonnier <emric.pichonnier@lenra.me>
2022-06-03 15:41:48 +0200

    feat: add AppGuardian (#167)

---
+++ lib/session/session_state_service.ex
@@ -0,0 +1,25 @@
+defmodule ApplicationRunner.Session.SessionStateServices do
+  @moduledoc """
+    Lenra.Sessionstate handle all operation for session state.
+  """
+
+  alias ApplicationRunner.{Guardian.AppGuardian, Services.TokenAgent, SessionSupervisor}
+
+  def create_token(session_id, user_id, env_id) do
+    with {:ok, token, _claims} <-
+           AppGuardian.encode_and_sign(session_id, %{
+             type: "session",
+             user_id: user_id,
+             env_id: env_id
+           }) do
+      {:ok, token}
+    end
+  end
+
+  def fetch_token(session_id) do
+    with agent <-
+           SessionSupervisor.fetch_module_pid!(session_id, TokenAgent) do
+      Agent.get(agent, fn state -> state end)
+    end
+  end
+end
