bdbca2fe3d6354a8250406933e44bb51484edc03
ClementGld <clement.guillaud@lenra.io>
2022-06-14 16:59:13 +0200

    fix problem send context

--- lib/session/widget/widget_cache.ex
+++ lib/session/widget/widget_cache.ex
@@ -49,7 +49,8 @@ defmodule ApplicationRunner.Widget.Cache do
         session_state,
         current_widget.name,
         current_widget.data,
-        current_widget.props
+        current_widget.props,
+        current_widget.context
       ],
       current_widget.id
     )
@@ -117,7 +118,7 @@ defmodule ApplicationRunner.Widget.Cache do
 
     context =
       if context_bool do
-        session_state.assigns |> Map.get("context")
+        session_state.assigns |> Map.get(:context, %{})
       else
         %{}
       end
