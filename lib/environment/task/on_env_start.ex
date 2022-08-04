700e88e4f9b16d213f942f3f628fdc719de9f40e
Emric Pichonnier <emric.pichonnier@lenra.me>
2022-08-04 16:07:34 +0200

    feat: add OnEnvStart Event (#204)

---
+++ lib/environment/task/on_env_start.ex
@@ -0,0 +1,18 @@
+defmodule ApplicationRunner.Environments.Task.OnEnvStart do
+  @moduledoc """
+    OnEnvStart task send listeners onEnvStart
+  """
+  use Task
+
+  alias ApplicationRunner.ApplicationServices
+
+  @on_env_start_action "onEnvStart"
+
+  def start_link(arg) do
+    Task.start_link(__MODULE__, :run, [arg])
+  end
+
+  def run(state) do
+    ApplicationServices.run_listener(state, @on_env_start_action, %{}, %{})
+  end
+end
