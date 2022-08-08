0a6e96df4d890cbc475ac1140bfb066c2154aa39
Emric Pichonnier <emric.pichonnier@lenra.io>
2022-08-08 15:34:07 +0200

    feat: fix empty context

--- lib/session/widget/widget_cache.ex
+++ lib/session/widget/widget_cache.ex
@@ -118,7 +118,7 @@ defmodule ApplicationRunner.Widget.Cache do
 
     context =
       if context_bool do
-        widget_context.context
+        session_state.context
       else
         %{}
       end
