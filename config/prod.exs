3aed78845058997ee542deea9e3e72af746fa734
ClementGld <clement.guillaud@lenra.io>
2022-08-10 16:53:10 +0200

    fix prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -1,9 +1,1 @@
 import Config
-
-config :lenra, Mongo.Repo,
-  username: System.fetch_env!("MONGO_USERNAME"),
-  password: System.fetch_env!("MONGO_PASSWORD"),
-  hostname: System.fetch_env!("MONGO_HOST")
-
-config :application_runner,
-  env: Application.fetch_env!("ENV")
