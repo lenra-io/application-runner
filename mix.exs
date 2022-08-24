b2c980601052c8feb8fb268a4b5194d4e1e953bc
Thomas DA ROCHA <8969556+taorepoara@users.noreply.github.com>
2022-08-24 07:42:49 +0200

    fix: Not user specific auth (#215)
    * fix: Not user specific auth
    
    * fix: Format

--- mix.exs
+++ mix.exs
@@ -50,14 +50,16 @@ defmodule ApplicationRunner.MixProject do
         host: "github.com",
         project: "lenra-io/query-parser.git",
         tag: "v1.0.0-beta.3",
-        credentials: "shiipou:#{System.get_env("GH_PERSONNAL_TOKEN")}"
+        credentials:
+          "#{System.get_env("GITHUB_AUTH", "shiipou:#{System.get_env("GH_PERSONNAL_TOKEN")}")}"
       ),
       private_git(
         name: :lenra_common,
         host: "github.com",
         project: "lenra-io/lenra-common.git",
         tag: "v2.0.4",
-        credentials: "shiipou:#{System.get_env("GH_PERSONNAL_TOKEN")}"
+        credentials:
+          "#{System.get_env("GITHUB_AUTH", "shiipou:#{System.get_env("GH_PERSONNAL_TOKEN")}")}"
       )
     ]
   end
