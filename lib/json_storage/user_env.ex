8c2741faa777dfdf44f26217ac4f5f834d513321
Louis G <louis@lenra.me>
2022-08-29 18:16:33 +0200

    wip

--- lib/json_storage/user_env.ex
+++ lib/json_storage/user_env.ex
@@ -6,42 +6,19 @@ defmodule ApplicationRunner.Contract.UserEnv do
 
   use Ecto.Schema
   import Ecto.Changeset
+  alias ApplicationRunner.Contract.{Environment, User}
 
-  alias ApplicationRunner.JsonStorage.UserData
-
-  @table_name Application.compile_env!(:application_runner, :lenra_user_table)
-  schema @table_name do
-    has_many(:user_datas, UserData, foreign_key: :user_id)
-    field(:mongo_user_id, Ecto.UUID, autogenerate: true)
+  @primary_key {:mongo_user_id, Ecto.UUID, autogenerate: true}
+  schema "user_env" do
+    belongs_to(:user, User)
+    belongs_to(:environment, Environment)
     timestamps()
   end
 
   def changeset(user, params \\ %{}) do
     user
-    |> cast(params, [:email])
-    |> validate_required([:email])
-  end
-
-  def embed(user) do
-    user_map =
-      if is_struct(user) do
-        user |> Map.from_struct()
-      else
-        user
-      end
-
-    changeset =
-      %__MODULE__{}
-      |> cast(user_map, [:id, :email, :inserted_at, :updated_at])
-      |> validate_required([:id, :email, :inserted_at, :updated_at])
-      |> unique_constraint(:email)
-      |> unique_constraint(:id)
-
-    if changeset.valid? do
-      Ecto.Changeset.apply_changes(changeset)
-    else
-      changeset
-    end
+    |> cast(params, [])
+    |> validate_required([])
   end
 
   def new(params) do
--- priv/repo/migrations/20220829145816_user_env_table.exs
+++ priv/repo/migrations/20220829145816_user_env_table.exs
@@ -3,15 +3,14 @@ defmodule ApplicationRunner.Repo.Migrations.UserEnvTable do
 
   def change do
 
-    create table(:user_env) do
-      add(:mongo_user_id, :uuid, null: false)
+    create table(:user_env, primary_key: false) do
+      add(:mongo_user_id, :uuid, primary_key: true, null: false)
       add(:environment_id, references(:environments), null: false)
       add(:user_id, references(:users), null: false)
       timestamps()
     end
 
     create(unique_index(:user_env, [:user_id, :environment_id], name: :user_env_user_id_environment_id))
-    create(unique_index(:user_env, [:mongo_user_id], name: :user_env_mongo_user_id))
 
   end
 end
