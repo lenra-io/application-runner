b5c6b8d300ddd842c5089c4a22bb19953a1aa927
Louis G <louis.guilbert@lenra.io>
2022-08-30 11:11:04 +0200

    feat: mongo_user_id (#220)

--- lib/controllers/datastore_controller.ex
+++ lib/controllers/datastore_controller.ex
@@ -1,24 +1,27 @@
 defmodule ApplicationRunner.DatastoreController do
   use ApplicationRunner, :controller
 
-  alias ApplicationRunner.{Guardian.AppGuardian.Plug, JsonStorage}
+  # alias ApplicationRunner.{Guardian.AppGuardian.Plug, JsonStorage}
 
-  def create(conn, params) do
-    with session_assigns <- Plug.current_resource(conn),
-         {:ok, %{inserted_datastore: datastore}} <-
-           JsonStorage.create_datastore(session_assigns.environment.id, params) do
-      conn
-      |> assign_data(datastore)
-      |> reply
-    end
+  def create(conn, _params) do
+    # with session_assigns <- Plug.current_resource(conn),
+    #      {:ok, %{inserted_datastore: datastore}} <-
+    #        JsonStorage.create_datastore(session_assigns.environment.id, params) do
+    #   conn
+    #   |> assign_data(datastore)
+    #   |> reply
+    # end
+    reply(conn)
   end
 
-  def delete(conn, params) do
-    with {:ok, %{deleted_datastore: datastore}} <-
-           JsonStorage.delete_datastore(params["datastore"]) do
-      conn
-      |> assign_data(datastore)
-      |> reply
-    end
+  def delete(conn, _params) do
+    #   with {:ok, %{deleted_datastore: datastore}} <-
+    #          JsonStorage.delete_datastore(params["datastore"]) do
+    #     conn
+    #     |> assign_data(datastore)
+    #     |> reply
+    #   end
+    # end
+    reply(conn)
   end
 end
---
+++ lib/mongo_storage.ex
@@ -0,0 +1,74 @@
+defmodule ApplicationRunner.MongoStorage do
+  @moduledoc """
+    ApplicationRunner.JsonStorage handles all data logic.
+    We can find delegate function to associate Services.
+  """
+
+  # alias ApplicationRunner.JsonStorage.Services
+
+  ########
+  # DATA #
+  ########
+
+  # defdelegate create_data(environment_id, params), to: Services.Data, as: :create
+
+  # defdelegate parse_and_exec_query(env_id, coll, query),
+  #   to: Services.Data,
+  #   as: :parse_and_exec_query
+
+  # defdelegate get_data(env_id, coll, data_id), to: Services.Data, as: :get
+
+  # defdelegate get_all_data(env_id, coll), to: Services.Data, as: :get_all
+
+  # defdelegate get_current_user_data(env_id, user_id),
+  #   to: Services.Data,
+  #   as: :get_current_user_data
+
+  # defdelegate update_data(environment_id, params), to: Services.Data, as: :update
+
+  # defdelegate delete_data(environment_id, params), to: Services.Data, as: :delete
+
+  #############
+  # DATASTORE #
+  #############
+
+  # defdelegate create_datastore(environment_id, params), to: Services.Datastore, as: :create
+
+  # defdelegate update_datastore(datastore_id, params), to: Services.Datastore, as: :update
+
+  # defdelegate delete_datastore(datastore_id), to: Services.Datastore, as: :delete
+
+  #############
+  # USERDATA #
+  #############
+
+  # defdelegate create_user_data(params), to: Services.UserData, as: :create
+
+  # defdelegate create_user_data_with_data(env_id, user_id),
+  #   to: Services.UserData,
+  #   as: :create_with_data
+
+  # defdelegate has_user_data?(env_id, user_id), to: Services.UserData, as: :has_user_data?
+
+  # defdelegate current_user_data_query(env_id, user_id),
+  #   to: Services.UserData,
+  #   as: :current_user_data_query
+
+  ##################
+  # DATA REFERENCE #
+  ##################
+
+  # defdelegate create_reference(params), to: Services.DataReferences, as: :create
+
+  # defdelegate delete_reference(params),
+  #   to: Services.DataReferences,
+  #   as: :delete
+
+  ##############
+  # QUERY VIEW #
+  ##############
+
+  # defdelegate get_one(env_id, datastore_name, data_id), to: Services.DataQueryView, as: :get_one
+
+  # defdelegate get_all(env_id, datastore_name), to: Services.DataQueryView, as: :get_all
+end
