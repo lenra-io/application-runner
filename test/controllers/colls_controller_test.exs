cd1cd23b08b534eac5664275ee7918fce565398e
Louis G <louis@lenra.me>
2022-08-31 10:42:41 +0200

    add colls test

---
+++ test/controllers/colls_controller_test.exs
@@ -0,0 +1,60 @@
+defmodule ApplicationRunner.CollsControllerTest do
+  use ApplicationRunner.ConnCase
+
+  alias ApplicationRunner.Contract
+  alias ApplicationRunner.Environment
+
+  @coll "controller_test"
+
+  setup ctx do
+    Application.ensure_all_started(:postgrex)
+    {:ok, _} = start_supervised(ApplicationRunner.FakeEndpoint)
+    start_supervised(ApplicationRunner.Repo)
+
+    {:ok, env} = ApplicationRunner.Repo.insert(Contract.Environment.new(%{}))
+
+    token = ApplicationRunner.AppChannel.do_create_env_token(env.id) |> elem(1)
+
+    env_metadata = %Environment.Metadata{
+      env_id: env.id,
+      function_name: "",
+      token: token
+    }
+
+    {:ok, _} = start_supervised({Environment.MetadataAgent, env_metadata})
+    {:ok, pid} = start_supervised({Mongo, Environment.MongoInstance.config(env.id)})
+
+    Mongo.drop_collection(pid, @coll)
+
+    doc_id =
+      Mongo.insert_one!(pid, @coll, %{"foo" => "bar"})
+      |> Map.get(:inserted_id)
+      |> BSON.ObjectId.encode!()
+
+    {:ok, Map.merge(ctx, %{mongo_pid: pid, token: token, doc_id: doc_id})}
+  end
+
+  describe "ApplicationRunner.CollsController.delete" do
+    test "should be protected with a token", %{conn: conn} do
+      conn = delete(conn, Routes.colls_path(conn, :delete, @coll))
+
+      assert %{"message" => _, "reason" => "unauthenticated"} = json_response(conn, 401)
+    end
+
+    test "Should delete the coll", %{conn: conn, token: token} do
+      conn =
+        conn
+        |> Plug.Conn.put_req_header("authorization", "Bearer " <> token)
+        |> delete(Routes.colls_path(conn, :delete, @coll))
+
+      assert %{} = json_response(conn, 200)
+
+      conn =
+        Phoenix.ConnTest.build_conn()
+        |> Plug.Conn.put_req_header("authorization", "Bearer " <> token)
+        |> get(Routes.docs_path(conn, :get_all, @coll))
+
+      assert %{"data" => []} = json_response(conn, 200)
+    end
+  end
+end
