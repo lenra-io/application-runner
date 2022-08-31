0a52c15c017343d39e7a09edd91a59965f0f1079
Emric Pichonnier <emric.pichonnier@lenra.me>
2022-08-31 09:01:55 +0200

    feat: some fix

--- lib/session/supervisor.ex
+++ lib/session/supervisor.ex
@@ -26,10 +26,7 @@ defmodule ApplicationRunner.Session.Supervisor do
       # ApplicationRunner.EventHandler
       # Event.OnUserFirstJoin
       {Session.Task.OnSessionStart,
-       session_id: session_metadata.session_id,
-       env_id: session_metadata.env_id,
-       token: session_metadata.token,
-       fucntion_name: session_metadata.function_name},
+       token: session_metadata.token, fucntion_name: session_metadata.function_name},
       {Session.UiServer, session_id: session_metadata.session_id}
     ]
 
