0564452a68704509b7654ea36436cba116e33a5c
jonas-martinez <cbobbxta.fr@gmail.com>
2022-09-14 20:45:01 +0200

    feat: Webhook Services

---
+++ lib/services/webhook_services.ex
@@ -0,0 +1,16 @@
+defmodule ApplicationRunner.WebhookServices do
+  @moduledoc """
+    The service that manages the webhooks.
+  """
+
+  alias ApplicationRunner.Webhooks.Webhook
+
+  def create(env_id, params) do
+  end
+
+  def get(env_id) do
+  end
+
+  def trigger(webhook_uuid) do
+  end
+end
