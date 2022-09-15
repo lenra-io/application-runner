e4cd2cf09a1856bde739cc5bdeb18a8f4eceb675
jonas-martinez <cbobbxta.fr@gmail.com>
2022-09-15 21:39:18 +0200

    feat: Webhook Controller

---
+++ lib/controllers/webhook_controller.ex
@@ -0,0 +1,12 @@
+defmodule ApplicationRunner.WebhookController do
+  use ApplicationRunner, :controller
+
+  def create(_conn, _params) do
+  end
+
+  def index(_conn, _params) do
+  end
+
+  def trigger(_conn, _params) do
+  end
+end
