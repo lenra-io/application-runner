667aa009a5182d592de39480b3d2c3a6efde1d5d
jonas-martinez <cbobbxta.fr@gmail.com>
2022-09-15 21:46:09 +0200

    feat: Webhook Controller - `create` implementation

--- lib/controllers/webhook_controller.ex
+++ lib/controllers/webhook_controller.ex
@@ -1,7 +1,14 @@
 defmodule ApplicationRunner.WebhookController do
   use ApplicationRunner, :controller
 
-  def create(_conn, _params) do
+  alias ApplicationRunner.WebhookServices
+
+  def create(conn, %{"environment_id" => env_id} = params) do
+    with {:ok, webhook} <- WebhookServices.create(env_id, params) do
+      conn
+      |> assign_data(webhook)
+      |> reply
+    end
   end
 
   def index(_conn, _params) do
