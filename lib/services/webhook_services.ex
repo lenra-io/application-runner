2031aa4608b74aac62aece1a6d3718fe3baaf836
jonas-martinez <cbobbxta.fr@gmail.com>
2022-09-16 10:10:26 +0200

    feat: Webhook Service - `trigger` implementation

--- lib/services/webhook_services.ex
+++ lib/services/webhook_services.ex
@@ -3,7 +3,10 @@ defmodule ApplicationRunner.WebhookServices do
     The service that manages the webhooks.
   """
 
+  alias ApplicationRunner.ApplicationServices
+  alias ApplicationRunner.Repo
   alias ApplicationRunner.Webhooks.Webhook
+
 
   def create(env_id, params) do
   end
@@ -11,6 +14,9 @@ defmodule ApplicationRunner.WebhookServices do
   def get(env_id) do
   end
 
-  def trigger(webhook_uuid) do
+  def trigger(webhook_uuid, payload) do
+    webhook = Repo.get(Webhook, webhook_uuid)
+
+    ApplicationServices.run_listener(<state>, webhook.action, payload, %{})
   end
 end
