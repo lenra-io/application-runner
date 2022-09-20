6f11c25d19eccef86cb1f0045ebe6a37088945f7
jonas-martinez <cbobbxta.fr@gmail.com>
2022-09-20 09:40:03 +0200

    Added test for webhook service

--- test/webhooks/webhook_services_test.exs
+++ test/webhooks/webhook_services_test.exs
@@ -24,17 +24,6 @@ defmodule ApplicationRunner.Webhooks.WebhookTest do
     {:ok, env_id: env.id}
   end
 
-  test "Webhook get should work properly", %{env_id: env_id} do
-    assert {:ok, webhook} =
-             Webhook.new(env_id, %{"action" => "listener"})
-             |> Repo.insert()
-
-    bypass = Bypass.open()
-    Bypass.stub(bypass, "POST", "/function/test", &handle_request(&1))
-
-    WebhookServices.trigger(webhook.uuid, %{})
-  end
-
   defp handle_request(conn) do
     {:ok, body, conn} = Plug.Conn.read_body(conn)
 
@@ -46,23 +35,22 @@ defmodule ApplicationRunner.Webhooks.WebhookTest do
       end
 
     case body_decoded do
-      # Manifest no body
-      "" ->
-        Plug.Conn.resp(conn, 200, Jason.encode!(%{"manifest" => @manifest}))
-
       # Listeners "action" in body
       %{"action" => _action} ->
         Plug.Conn.resp(conn, 200, "")
-
-      # Widget data key
-      %{"data" => data, "props" => props, "widget" => widget} ->
-        {:ok, widget} = ApplicationRunnerAdapter.get_widget(%{}, widget, data, props)
-
-        Plug.Conn.resp(
-          conn,
-          200,
-          Jason.encode!(widget)
-        )
     end
+  end
+
+  test "Webhook get should properly trigger listener", %{env_id: env_id} do
+    assert {:ok, webhook} =
+             Webhook.new(env_id, %{"action" => "listener"})
+             |> Repo.insert()
+
+    bypass = Bypass.open(port: 1234)
+    Bypass.stub(bypass, "POST", "/function/test", &handle_request(&1))
+
+    res = WebhookServices.trigger(webhook.uuid, %{})
+
+    assert res == :ok
   end
 end
