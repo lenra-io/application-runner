71e71d845b6792234cf7f3ee9f33f7956d050450
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-09-21 10:32:22 +0200

    feat: Webhook Service - `Trigger` implementation (#235)

---
+++ lib/webhooks/webhook_services.ex
@@ -0,0 +1,35 @@
+defmodule ApplicationRunner.Webhooks.WebhookServices do
+  @moduledoc """
+    The service that manages the webhooks.
+  """
+
+  alias ApplicationRunner.ApplicationServices
+  alias ApplicationRunner.Environment.MetadataAgent
+  alias ApplicationRunner.Errors.TechnicalError
+  alias ApplicationRunner.Repo
+  alias ApplicationRunner.Webhooks.Webhook
+
+  def create(env_id, params) do
+  end
+
+  def get(env_id) do
+  end
+
+  def trigger(webhook_uuid, payload) do
+    case Repo.get(Webhook, webhook_uuid) do
+      nil ->
+        TechnicalError.error_404_tuple()
+
+      webhook ->
+        metadata = MetadataAgent.get_metadata(webhook.environment_id)
+
+        ApplicationServices.run_listener(
+          metadata.function_name,
+          webhook.action,
+          webhook.props,
+          payload,
+          metadata.token
+        )
+    end
+  end
+end
---
+++ test/webhooks/webhook_services_test.exs
@@ -0,0 +1,98 @@
+defmodule ApplicationRunner.Webhooks.ServicesTest do
+  @moduledoc false
+
+  use ApplicationRunner.RepoCase
+
+  alias ApplicationRunner.Contract.{Environment, User}
+  alias ApplicationRunner.Environment.{Metadata, MetadataAgent}
+  alias ApplicationRunner.Repo
+  alias ApplicationRunner.Webhooks.{Webhook, WebhookServices}
+
+  setup do
+    {:ok, env} = Repo.insert(Environment.new())
+    token = ApplicationRunner.AppChannel.do_create_env_token(env.id) |> elem(1)
+
+    env_metadata = %Metadata{
+      env_id: env.id,
+      function_name: "test",
+      token: token
+    }
+
+    {:ok, _} = start_supervised({MetadataAgent, env_metadata})
+
+    bypass = Bypass.open(port: 1234)
+
+    {:ok, %{env_id: env.id, bypass: bypass}}
+  end
+
+  defp handle_request(conn, callback) do
+    {:ok, body, conn} = Plug.Conn.read_body(conn)
+
+    body_decoded =
+      if String.length(body) != 0 do
+        Jason.decode!(body)
+      else
+        ""
+      end
+
+    callback.(body_decoded)
+
+    case body_decoded do
+      # Listeners "action" in body
+      %{"action" => _action} ->
+        Plug.Conn.resp(conn, 200, "")
+    end
+  end
+
+  test "Webhook should properly trigger listener", %{env_id: env_id, bypass: bypass} do
+    assert {:ok, webhook} =
+             Webhook.new(env_id, %{"action" => "listener", "props" => %{"propKey" => "propValue"}})
+             |> Repo.insert()
+
+    Bypass.stub(
+      bypass,
+      "POST",
+      "/function/test",
+      &handle_request(&1, fn body ->
+        assert body["props"] == %{"propKey" => "propValue"}
+        assert body["action"] == "listener"
+        assert body["event"] == %{"eventPropKey" => "eventPropValue"}
+      end)
+    )
+
+    assert :ok == WebhookServices.trigger(webhook.uuid, %{"eventPropKey" => "eventPropValue"})
+  end
+
+  test "Trigger not existing webhook should return an error", %{env_id: _env_id, bypass: _bypass} do
+    assert {:error, %LenraCommon.Errors.TechnicalError{reason: :error_404}} =
+             WebhookServices.trigger(Ecto.UUID.generate(), %{})
+  end
+
+  test "User specific Webhook should properly trigger listener", %{env_id: env_id, bypass: bypass} do
+    user =
+      %{email: "test@test.te"}
+      |> User.new()
+      |> Repo.insert!()
+
+    assert {:ok, webhook} =
+             Webhook.new(env_id, %{
+               "user_id" => user.id,
+               "action" => "listener",
+               "props" => %{"propKey" => "propValue"}
+             })
+             |> Repo.insert()
+
+    Bypass.stub(
+      bypass,
+      "POST",
+      "/function/test",
+      &handle_request(&1, fn body ->
+        assert body["props"] == %{"propKey" => "propValue"}
+        assert body["action"] == "listener"
+        assert body["event"] == %{"eventPropKey" => "eventPropValue"}
+      end)
+    )
+
+    assert :ok == WebhookServices.trigger(webhook.uuid, %{"eventPropKey" => "eventPropValue"})
+  end
+end
