b09c02e24a89ca8cdf8c121fb90855ec8ccb4fb2
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-09-21 15:40:52 +0200

    feat: Webhook Services (#230)

---
+++ lib/webhooks/webhook_services.ex
@@ -0,0 +1,58 @@
+defmodule ApplicationRunner.Webhooks.WebhookServices do
+  @moduledoc """
+    The service that manages the webhooks.
+  """
+
+  import Ecto.Query, only: [from: 2]
+
+  alias ApplicationRunner.ApplicationServices
+  alias ApplicationRunner.Environment.MetadataAgent
+  alias ApplicationRunner.Errors.TechnicalError
+  alias ApplicationRunner.Repo
+  alias ApplicationRunner.Webhooks.Webhook
+
+  def create(env_id, params) do
+    Webhook.new(env_id, params)
+    |> Repo.insert()
+  end
+
+  def app_create(%{env: %ApplicationRunner.Contract.Environment{id: env_id}}, params) do
+    create(env_id, params)
+  end
+
+  def app_create(
+        %{
+          env: %ApplicationRunner.Contract.Environment{id: env_id},
+          user: %ApplicationRunner.Contract.User{id: user_id}
+        },
+        params
+      ) do
+    create(env_id, Map.merge(params, %{"user_id" => user_id}))
+  end
+
+  def get(env_id) do
+    Repo.all(from(w in Webhook, where: w.environment_id == ^env_id))
+  end
+
+  def get(env_id, user_id) do
+    Repo.all(from(w in Webhook, where: w.environment_id == ^env_id and w.user_id == ^user_id))
+  end
+
+  def trigger(webhook_uuid, payload) do
+    case Repo.get(Webhook, webhook_uuid) do
+      nil ->
+        TechnicalError.error_404_tuple()
+
+      webhook ->
+        metadata = MetadataAgent.get_metadata(webhook.environment_id)
+
+        ApplicationServices.run_listener(
+          metadata.function_name,
+          webhook.action,
+          webhook.props,
+          payload,
+          metadata.token
+        )
+    end
+  end
+end
