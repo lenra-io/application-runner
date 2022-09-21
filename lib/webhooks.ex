623b788eb5a249b052b655bd9e797ca4f6e748ab
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-09-21 15:56:12 +0200

    feat: Add `app_create` defdelegate for webhooks (#243)

--- lib/webhooks.ex
+++ lib/webhooks.ex
@@ -6,6 +6,7 @@ defmodule ApplicationRunner.Webhooks do
   alias ApplicationRunner.Webhooks
 
   defdelegate create(env_id, params), to: Webhooks.WebhookServices
+  defdelegate app_create(token_params, params), to: Webhooks.WebhookServices
   defdelegate get(env_id), to: Webhooks.WebhookServices
   defdelegate trigger(webhook_uuid, payload), to: Webhooks.WebhookServices
 
