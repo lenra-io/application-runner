63728b482574aafb3e1bddeed38dddf231c10c10
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-09-21 15:32:14 +0200

    feat: Webhook Service - `Create` implementation (#231)

--- test/webhooks/webhook_services_test.exs
+++ test/webhooks/webhook_services_test.exs
@@ -20,9 +20,14 @@ defmodule ApplicationRunner.Webhooks.ServicesTest do
 
     {:ok, _} = start_supervised({MetadataAgent, env_metadata})
 
+    user =
+      %{email: "test@test.te"}
+      |> User.new()
+      |> Repo.insert!()
+
     bypass = Bypass.open(port: 1234)
 
-    {:ok, %{env_id: env.id, bypass: bypass}}
+    {:ok, %{env_id: env.id, user_id: user.id, bypass: bypass}}
   end
 
   defp handle_request(conn, callback) do
@@ -44,7 +49,11 @@ defmodule ApplicationRunner.Webhooks.ServicesTest do
     end
   end
 
-  test "Webhook should properly trigger listener", %{env_id: env_id, bypass: bypass} do
+  test "Webhook should properly trigger listener", %{
+    env_id: env_id,
+    user_id: _user_id,
+    bypass: bypass
+  } do
     assert {:ok, webhook} =
              Webhook.new(env_id, %{"action" => "listener", "props" => %{"propKey" => "propValue"}})
              |> Repo.insert()
@@ -63,20 +72,23 @@ defmodule ApplicationRunner.Webhooks.ServicesTest do
     assert :ok == WebhookServices.trigger(webhook.uuid, %{"eventPropKey" => "eventPropValue"})
   end
 
-  test "Trigger not existing webhook should return an error", %{env_id: _env_id, bypass: _bypass} do
+  test "Trigger not existing webhook should return an error", %{
+    env_id: _env_id,
+    user_id: _user_id,
+    bypass: _bypass
+  } do
     assert {:error, %LenraCommon.Errors.TechnicalError{reason: :error_404}} =
              WebhookServices.trigger(Ecto.UUID.generate(), %{})
   end
 
-  test "User specific Webhook should properly trigger listener", %{env_id: env_id, bypass: bypass} do
-    user =
-      %{email: "test@test.te"}
-      |> User.new()
-      |> Repo.insert!()
-
+  test "User specific Webhook should properly trigger listener", %{
+    env_id: env_id,
+    user_id: user_id,
+    bypass: bypass
+  } do
     assert {:ok, webhook} =
              Webhook.new(env_id, %{
-               "user_id" => user.id,
+               "user_id" => user_id,
                "action" => "listener",
                "props" => %{"propKey" => "propValue"}
              })
@@ -94,6 +106,48 @@ defmodule ApplicationRunner.Webhooks.ServicesTest do
     )
 
     assert :ok == WebhookServices.trigger(webhook.uuid, %{"eventPropKey" => "eventPropValue"})
+  end
+
+  test "Webhook create should work properly", %{
+    env_id: env_id,
+    user_id: _user_id,
+    bypass: _bypass
+  } do
+    assert {:ok, _webhook} = WebhookServices.create(env_id, %{"action" => "listener"})
+
+    webhook = Enum.at(Repo.all(Webhook), 0)
+
+    assert webhook.action == "listener"
+    assert webhook.environment_id == env_id
+  end
+
+  test "Webhook create with user should work", %{
+    env_id: env_id,
+    user_id: user_id,
+    bypass: _bypass
+  } do
+    assert {:ok, webhook} =
+             WebhookServices.create(env_id, %{"action" => "listener", "user_id" => user_id})
+
+    webhook_preload = Repo.preload(webhook, :user)
+
+    assert webhook_preload.user.id == user_id
+  end
+
+  test "Webhook create without action should not work", %{
+    env_id: env_id,
+    user_id: _user_id,
+    bypass: _bypass
+  } do
+    assert {:error, _reason} = WebhookServices.create(env_id, %{})
+  end
+
+  test "Webhook create with invalid env_id should not work", %{
+    env_id: _env_id,
+    user_id: _user_id,
+    bypass: _bypass
+  } do
+    assert {:error, _reason} = WebhookServices.create(-1, %{"action" => "listener"})
   end
 
   test "Webhook get should work properly", %{env_id: env_id} do
