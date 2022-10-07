739786b26348e453f20e7475f33b259de5bff124
jonas-martinez <cbobbxta.fr@gmail.com>
2022-10-07 10:54:45 +0200

    fix: Update dependencies

--- mix.exs
+++ mix.exs
@@ -46,8 +46,8 @@ defmodule ApplicationRunner.MixProject do
       {:finch, "~> 0.12"},
       {:bypass, "~> 2.0", only: :test},
       {:mongodb_driver, "~> 0.9.1"},
-      {:query_parser, git: "https://github.com/lenra-io/query-parser.git", tag: "v1.0.0-beta.12"},
-      {:lenra_common, git: "https://github.com/lenra-io/lenra-common.git", tag: "v2.3.0"}
+      {:query_parser, git: "https://github.com/lenra-io/query-parser.git", tag: "v1.0.0-beta.13"},
+      {:lenra_common, git: "https://github.com/lenra-io/lenra-common.git", tag: "v2.3.1"}
     ]
   end
 
