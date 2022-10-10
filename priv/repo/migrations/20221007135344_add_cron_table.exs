0b671dffbf5c97a6b6dccef7cedf025d60f14125
jonas-martinez <cbobbxta.fr@gmail.com>
2022-10-10 10:12:31 +0200

    null false on cron

--- priv/repo/migrations/20221007135344_add_cron_table.exs
+++ priv/repo/migrations/20221007135344_add_cron_table.exs
@@ -6,8 +6,8 @@ defmodule ApplicationRunner.Repo.Migrations.AddCronTable do
       add(:environment_id, references(:environments), null: false)
       add(:user_id, references(:users))
       add(:listener_name, :string, null: false)
+      add(:cron, :string, null: false)
       add(:props, :map)
-      add(:cron, :string)
 
       timestamps()
     end
