b566e667eac195f36bd244f064f50a3769dc5ac3
jonas-martinez <cbobbxta.fr@gmail.com>
2022-10-18 16:25:23 +0200

    Add config

--- config/config.exs
+++ config/config.exs
@@ -25,6 +25,9 @@ config :application_runner, ApplicationRunner.Repo,
   database: "file::memory:?cache=shared",
   log: false
 
+config :application_runner, ApplicationRunner.Scheduler,
+  storage: ApplicationRunner.Storage
+
 config :swarm,
   debug: false
 
