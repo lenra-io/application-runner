ec6c347a91b2dd4ba99ee8106e222da25dc800fb
jonas-martinez <cbobbxta.fr@gmail.com>
2022-11-07 15:39:14 +0100

    This should fix all tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,12 +1,13 @@
 Application.load(:application_runner)
 
-ApplicationRunner.Repo.start_link()
-Mix.Tasks.Ecto.Migrate.run([])
-Ecto.Adapters.SQL.Sandbox.mode(ApplicationRunner.Repo, :manual)
-
-# This start all the dependancy applications needed
 for app <- Application.spec(:application_runner, :applications) do
   Application.ensure_all_started(app)
 end
+
+ApplicationRunner.Repo.start_link()
+Mix.Tasks.Ecto.Migrate.run([])
+Ecto.Adapters.SQL.Sandbox.mode(ApplicationRunner.Repo, :auto)
+
+Application.ensure_started(:application_runner)
 
 ExUnit.start()
