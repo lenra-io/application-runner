c4fb855a272dc7ddfe919015a10670ef8fb203a2
jonas-martinez <cbobbxta.fr@gmail.com>
2022-11-09 13:32:34 +0100

    Rework migrations

--- priv/repo/migrations/20221007135344_add_cron_table.exs
+++ priv/repo/migrations/20221007135344_add_cron_table.exs
@@ -6,11 +6,15 @@ defmodule ApplicationRunner.Repo.Migrations.AddCronTable do
       add(:environment_id, references(:environments), null: false)
       add(:user_id, references(:users))
       add(:listener_name, :string, null: false)
-      add(:cron_expression, :string, null: false)
+      add(:schedule, :string, null: false)
       add(:props, :map)
 
       add(:should_run_missed_steps, :boolean, default: false)
-      add(:last_run_date, :date)
+
+      add :name, :string
+      add :overlap, :boolean
+      add :state, :string
+      add :timezone, :date
 
       timestamps()
     end
