fe8dd77d4dba33abe40aabf9f668f27cf1832532
jonas-martinez <cbobbxta.fr@gmail.com>
2022-11-09 14:05:58 +0100

    ApplicationRunner cannot restart crons because it does not have enough knowledge of the system

--- lib/scheduler.ex
+++ lib/scheduler.ex
@@ -1,12 +1,4 @@
 defmodule ApplicationRunner.Scheduler do
   @moduledoc false
   use Quantum, otp_app: :application_runner
-
-  require Logger
-
-  def init(opts) do
-    # TODO START ALL CRONS IN DATABASE HERE
-    IO.inspect("INIT SCHEDULER")
-    opts
-  end
 end
