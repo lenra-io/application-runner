6ef3cec0216fbd343231b525b6bb922be31c8730
Pichonnier Emric <emric.pichonnier@gmail.com>
2022-11-09 11:05:16 +0100

    fix: migration

--- priv/repo/migrations/20221108152648_listener_monitor.exs
+++ priv/repo/migrations/20221108152648_listener_monitor.exs
@@ -2,26 +2,25 @@ defmodule ApplicationRunner.Repo.Migrations.ListenerMonitor do
   use Ecto.Migration
 
   def change do
-        create table(:session_listener_measurement, primary_key: false) do
-          add(:uuid, :uuid, primary_key: true)
-          add(:session_mesureament_uuid, references(:session_measurement), null: false)
-          add(:start_time, :timestamp, null: false)
-          add(:end_time, :timestamp)
-          add(:duration, :integer)
+    create table(:session_listener_measurement, primary_key: false) do
+      add(:uuid, :uuid, primary_key: true)
+      add(:session_mesureament_uuid, references(:session_measurement), null: false)
+      add(:start_time, :timestamp, null: false)
+      add(:end_time, :timestamp)
+      add(:duration, :integer)
 
-          timestamps()
-        end
-
-        create table(:env_listener_measurement, primary_key: false) do
-          add(:uuid, :uuid, primary_key: true)
-          add(:environment_id, references(:environment), null: false)
-          add(:start_time, :timestamp, null: false)
-          add(:end_time, :timestamp)
-          add(:duration, :integer)
-
-          timestamps()
-        end
-
+      timestamps()
     end
+
+    create table(:env_listener_measurement, primary_key: false) do
+      add(:uuid, :uuid, primary_key: true)
+      add(:environment_id, references(:environment), null: false)
+      add(:start_time, :timestamp, null: false)
+      add(:end_time, :timestamp)
+      add(:duration, :integer)
+
+      timestamps()
+    end
+
   end
 end
