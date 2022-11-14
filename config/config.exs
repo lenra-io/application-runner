2aae8bfa86df0c1feaf619f8328ed5b64d0f675c
jonas-martinez <cbobbxta.fr@gmail.com>
2022-11-14 14:53:22 +0100

    fix again

--- config/config.exs
+++ config/config.exs
@@ -25,12 +25,8 @@ config :application_runner,
   adapter: ApplicationRunner.FakeAppAdapter
 
 config :application_runner, ApplicationRunner.Repo,
-  username: System.get_env("POSTGRES_USER", "postgres"),
-  password: System.get_env("POSTGRES_PASSWORD", "postgres"),
-  database: System.get_env("POSTGRES_DB", "lenra_dev"),
-  hostname: System.get_env("POSTGRES_HOST", "localhost"),
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  database: "file::memory:?cache=shared",
+  log: false
 
 config :application_runner, ApplicationRunner.Scheduler, storage: ApplicationRunner.Storage
 
