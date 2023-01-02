d6891d63bcb32fbc2fe8f436185f936d10146ce2
jonas-martinez <cbobbxta.fr@gmail.com>
2023-01-02 17:09:29 +0100

    Raise error on port invalid

--- lib/environment/mongo_instance.ex
+++ lib/environment/mongo_instance.ex
@@ -4,25 +4,34 @@ defmodule ApplicationRunner.Environment.MongoInstance do
   """
   @env Application.compile_env!(:application_runner, :env)
 
+  require Logger
+
   use SwarmNamed
 
   def config(env_id) do
     database_name = @env <> "_#{env_id}"
 
     mongo_config = Application.fetch_env!(:application_runner, :mongo)
-    {port, _} = Integer.parse(mongo_config[:port])
 
-    [
-      hostname: mongo_config[:hostname],
-      port: port,
-      database: database_name,
-      username: mongo_config[:username],
-      password: mongo_config[:password],
-      ssl: mongo_config[:ssl],
-      name: get_full_name(env_id),
-      auth_source: mongo_config[:auth_source],
-      pool_size: 10
-    ]
+    case Integer.parse(mongo_config[:port]) do
+      {port, _} ->
+        [
+          hostname: mongo_config[:hostname],
+          port: port,
+          database: database_name,
+          username: mongo_config[:username],
+          password: mongo_config[:password],
+          ssl: mongo_config[:ssl],
+          name: get_full_name(env_id),
+          auth_source: mongo_config[:auth_source],
+          pool_size: 10
+        ]
+
+      :error ->
+        error = "Failed to parse Mongo port: " <> mongo_config[:port]
+        Logger.emergency(error)
+        raise error
+    end
   end
 end
 
