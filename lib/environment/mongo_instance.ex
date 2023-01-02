39fd75e1742fc48b246fe1b61b78a8b356c1532a
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2023-01-02 17:13:46 +0100

    feat!: MongoDB connection options (#288)

--- lib/environment/mongo_instance.ex
+++ lib/environment/mongo_instance.ex
@@ -4,18 +4,34 @@ defmodule ApplicationRunner.Environment.MongoInstance do
   """
   @env Application.compile_env!(:application_runner, :env)
 
+  require Logger
+
   use SwarmNamed
 
   def config(env_id) do
     database_name = @env <> "_#{env_id}"
-    mongo_url = Application.fetch_env!(:application_runner, :mongo_url)
 
-    [
-      url: "#{mongo_url}/#{database_name}",
-      name: get_full_name(env_id),
-      auth_source: System.get_env("MONGO_AUTH_SOURCE", database_name),
-      pool_size: 10
-    ]
+    mongo_config = Application.fetch_env!(:application_runner, :mongo)
+
+    case Integer.parse(mongo_config[:port]) do
+      {port, _} ->
+        [
+          hostname: mongo_config[:hostname],
+          port: port,
+          database: database_name,
+          username: mongo_config[:username],
+          password: mongo_config[:password],
+          ssl: mongo_config[:ssl],
+          name: get_full_name(env_id),
+          auth_source: mongo_config[:auth_source],
+          pool_size: 10
+        ]
+
+      :error ->
+        error = "Failed to parse Mongo port: " <> mongo_config[:port]
+        Logger.emergency(error)
+        raise error
+    end
   end
 end
 
