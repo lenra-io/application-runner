d7db598e0b9bc3774522a7045e90db9f93eca04c
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2023-02-10 10:23:05 +0100

    Update lib/environment/metadata.ex

--- lib/environment/metadata.ex
+++ lib/environment/metadata.ex
@@ -1,6 +1,6 @@
 defmodule ApplicationRunner.Environment.Metadata do
   @moduledoc """
-    The Environmnet metadata.
+    The Environment metadata.
   """
   @enforce_keys [:env_id, :function_name, :token]
   defstruct [
