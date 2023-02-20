1265c1da27a4e4e1e524b69638b002984082c4d9
jonas-martinez <cbobbxta.fr@gmail.com>
2023-02-20 16:05:33 +0100

    Now partially working

--- lib/channels/routes_channel.ex
+++ lib/channels/routes_channel.ex
@@ -2,6 +2,7 @@ defmodule ApplicationRunner.RoutesChannel do
   @moduledoc """
     `ApplicationRunner.RoutesChannel` handles the app channel to run app and listeners and push to the user the resulted UI or Patch
   """
+  use SwarmNamed
 
   defmacro __using__(_opts) do
     quote do
@@ -20,10 +21,26 @@ defmodule ApplicationRunner.RoutesChannel do
       def join("routes", %{"mode" => "lenra"}, socket) do
         env_id = socket.assigns.env_id
         manifest = Environment.ManifestHandler.get_manifest(env_id)
+        session_id = socket.assigns.session_id
 
         res = %{"lenraRoutes" => Map.get(manifest, "lenraRoutes")}
 
-        {:ok, res, socket}
+        with :yes <- Swarm.register_name(get_swarm_name(session_id), self()) do
+          {:ok, res, socket}
+        else
+          :no ->
+            Logger.critical(
+              BusinessError.could_not_register_appchannel(%{
+                session_id: session_id,
+                route: "routes"
+              })
+            )
+
+            {:error, DevError.message("Could not register the AppChannel into swarm")}
+
+          err ->
+            err
+        end
       end
 
       def join("routes", %{"mode" => "json"}, socket) do
@@ -37,6 +54,21 @@ defmodule ApplicationRunner.RoutesChannel do
 
       def join(_, _any, _socket) do
         {:error, ErrorHelpers.translate_error(BusinessError.invalid_channel_name())}
+      end
+
+      ########
+      # INFO #
+      ########
+
+      # Send new route to client
+      def handle_info({:send, :navTo, route}, socket) do
+        Logger.debug("send route #{inspect(route)}")
+        push(socket, "navTo", route)
+        {:noreply, socket}
+      end
+
+      def get_swarm_name(session_id) do
+        ApplicationRunner.RoutesChannel.get_name(session_id)
       end
     end
   end
