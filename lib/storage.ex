9982c9a44a07569261b1cefe17ab063637307f56
jonas-martinez <cbobbxta.fr@gmail.com>
2023-03-06 15:25:34 +0100

    fix

--- lib/storage.ex
+++ lib/storage.ex
@@ -24,17 +24,15 @@ defmodule ApplicationRunner.Storage do
   def add_job(_storage_pid, job) do
     IO.inspect("ADDING JOB")
 
-    IO.inspect(
-      with %Ecto.Changeset{valid?: true} = changeset <- Crons.to_changeset(job),
-           {:ok, _res} <-
-             Repo.insert(changeset,
-               on_conflict:
-                 {:replace,
-                  [:schedule, :listener_name, :props, :should_run_missed_steps, :overlap, :state]}
-             ) do
-        :ok
-      end
-    )
+    with %Ecto.Changeset{valid?: true} = changeset <- Crons.to_changeset(job),
+         {:ok, _res} <-
+           Repo.insert(changeset,
+             on_conflict:
+               {:replace,
+                [:schedule, :listener_name, :props, :should_run_missed_steps, :overlap, :state]}
+           ) do
+      :ok
+    end
   end
 
   @impl Storage
