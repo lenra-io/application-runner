0f95948a67e82a927ed7e5aaf1e1d935eb35faf7
jonas-martinez <cbobbxta.fr@gmail.com>
2023-03-06 14:35:48 +0100

    Comment dangerous functions

--- lib/controllers/cron_controller.ex
+++ lib/controllers/cron_controller.ex
@@ -19,20 +19,20 @@ defmodule ApplicationRunner.CronController do
     end
   end
 
-  def get(conn, %{"name" => cron_name} = _params) do
-    with {:ok, cron} <-
-           Crons.get_by_name(cron_name) do
-      reply(conn, cron)
-    end
-  end
+  # def get(conn, %{"name" => cron_name} = _params) do
+  #   with {:ok, cron} <-
+  #          Crons.get_by_name(cron_name) do
+  #     reply(conn, cron)
+  #   end
+  # end
 
-  def index(conn, %{"env_id" => env_id, "user_id" => user_id} = _params) do
-    reply(conn, Crons.all(env_id, user_id))
-  end
+  # def index(conn, %{"env_id" => env_id, "user_id" => user_id} = _params) do
+  #   reply(conn, Crons.all(env_id, user_id))
+  # end
 
-  def index(conn, %{"env_id" => env_id} = _params) do
-    reply(conn, Crons.all(env_id))
-  end
+  # def index(conn, %{"env_id" => env_id} = _params) do
+  #   reply(conn, Crons.all(env_id))
+  # end
 
   def index(conn, _params) do
     case AppGuardian.Plug.current_resource(conn) do
@@ -52,8 +52,6 @@ defmodule ApplicationRunner.CronController do
   end
 
   def delete(conn, %{"name" => name} = _params) do
-    IO.inspect("DELETE #{name}")
-
     {:ok, loaded_name} = ApplicationRunner.Ecto.Reference.load(name)
 
     with :ok <- Crons.delete(loaded_name) do
