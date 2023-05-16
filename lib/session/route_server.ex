fe75f9e8d897e9f8418fa2f9b097a62666fa5cea
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2023-05-16 10:08:40 +0200

    feat: Add @me and pathParams in context (#335)

--- lib/session/route_server.ex
+++ lib/session/route_server.ex
@@ -96,6 +96,7 @@ defmodule ApplicationRunner.Session.RouteServer do
          name <- Map.get(base_view, "name"),
          props <- Map.get(base_view, "props", %{}),
          find <- Map.get(base_view, "find", %{}),
+         context_projection <- Map.get(base_view, "context"),
          {coll, query, projection} <- extract_find(base_view, find),
          {:ok, view_uid} <-
            create_view_uid(
@@ -105,6 +106,7 @@ defmodule ApplicationRunner.Session.RouteServer do
              %{"route" => route_params},
              props,
              session_metadata.context,
+             context_projection,
              ""
            ) do
       builder_mod.build_ui(session_metadata, view_uid)
@@ -177,6 +179,7 @@ defmodule ApplicationRunner.Session.RouteServer do
           map(),
           map() | nil,
           map(),
+          map() | nil,
           binary()
         ) :: {:ok, ViewUid.t()} | {:error, LenraCommon.Errors.BusinessError.t()}
   def create_view_uid(
@@ -186,6 +189,7 @@ defmodule ApplicationRunner.Session.RouteServer do
         query_params,
         props,
         context,
+        context_projection,
         prefix_path
       ) do
     coll = Map.get(find, :coll)
@@ -197,6 +201,11 @@ defmodule ApplicationRunner.Session.RouteServer do
 
     params = query_params |> Map.merge(%{"me" => mongo_user_id})
     query_transformed = Parser.replace_params(query, params)
+
+    context =
+      context
+      |> Map.merge(%{"me" => mongo_user_id, "pathParams" => query_params["route"]})
+      |> project_map(context_projection)
 
     with {:ok, query_parsed} <- parse_query(query, params) do
       {:ok,
@@ -210,6 +219,28 @@ defmodule ApplicationRunner.Session.RouteServer do
          context: context,
          projection: projection
        }}
+    end
+  end
+
+  @doc """
+  Projects elements from the given map based on the provided projection.
+
+  ## Examples
+
+      iex> Projection.project_map(%{"foo" => "bar", "john" => "doe"}, %{"foo" => true})
+      %{"foo" => "bar"}
+
+  """
+  @spec project_map(map(), map() | nil) :: map()
+  def project_map(map, projection) do
+    case projection do
+      nil ->
+        %{}
+
+      _ ->
+        Enum.reduce(projection, %{}, fn {key, true}, acc ->
+          Map.put(acc, key, Map.get(map, key))
+        end)
     end
   end
 
