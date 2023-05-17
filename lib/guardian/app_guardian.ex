9eb2e5a8686f563c9682c5a66f79aa714e08612d
Emric Pichonnier <emric.pichonnier@gmail.com>
2023-05-17 11:02:19 +0200

    feat: add resource_from_claims

--- lib/guardian/app_guardian.ex
+++ lib/guardian/app_guardian.ex
@@ -19,6 +19,29 @@ defmodule ApplicationRunner.Guardian.AppGuardian do
     {:ok, to_string(session_pid)}
   end
 
+  def resource_from_claims(%{
+        "user_id" => user_id,
+        "env_id" => env_id,
+        "transaction_id" => transaction_id
+      }) do
+    env = MongoStorage.get_env!(env_id)
+    user = MongoStorage.get_user!(user_id)
+    mongo_user_link = MongoStorage.get_mongo_user_link!(env_id, user_id)
+
+    {:ok,
+     %{
+       environment: env,
+       user: user,
+       mongo_user_link: mongo_user_link,
+       transaction_id: transaction_id
+     }}
+  end
+
+  def resource_from_claims(%{"env_id" => env_id, "transaction_id" => transaction_id}) do
+    env = MongoStorage.get_env!(env_id)
+    {:ok, %{environment: env, transaction_id: transaction_id}}
+  end
+
   def resource_from_claims(%{"user_id" => user_id, "env_id" => env_id}) do
     env = MongoStorage.get_env!(env_id)
     user = MongoStorage.get_user!(user_id)
