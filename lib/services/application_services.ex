125c327994401e07b34b545477c8de9f2dfcc00c
jonas-martinez <cbobbxta.fr@gmail.com>
2023-05-17 16:31:09 +0200

    add inspect

--- lib/services/application_services.ex
+++ lib/services/application_services.ex
@@ -55,6 +55,8 @@ defmodule ApplicationRunner.ApplicationServices do
     peeked_token = AppGuardian.peek(token)
     start_time = Telemetry.start(:app_listener, peeked_token.claims)
 
+    IO.inspect(url)
+
     res =
       Finch.build(:post, url, headers, body)
       |> Finch.request(AppHttp,
