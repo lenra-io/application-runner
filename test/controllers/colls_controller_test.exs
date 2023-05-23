085bd310716c586dc78c66fb39b8df4b8936fd45
Emric Pichonnier <emric.pichonnier@lenra.io>
2023-05-23 09:35:21 +0200

    feat: mongo error rework (#333)

--- test/controllers/colls_controller_test.exs
+++ test/controllers/colls_controller_test.exs
@@ -63,5 +63,17 @@ defmodule ApplicationRunner.CollsControllerTest do
 
       assert [] = json_response(conn, 200)
     end
+
+    test "try delete unset coll", %{conn: conn, token: token} do
+      conn =
+        conn
+        |> Plug.Conn.put_req_header("authorization", "Bearer " <> token)
+        |> delete(Routes.colls_path(conn, :delete, "test"))
+
+      assert %{
+               "message" => "Could not access mongo. Please try again later.",
+               "metadata" => %{"code" => 26, "message" => "ns not found"}
+             } = json_response(conn, 400)
+    end
   end
 end
